a9f2b1e1faea15ff399305a1f45999d5ad36e734
Almurat <180107214@stu.sdu.edu.kz>
2022-09-12 23:43:23 +0600

    -v 0.2

--- lib/friends_page.dart
+++ lib/friends_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'main.dart';
 
 class FriendsPage extends StatefulWidget{
 
@@ -12,13 +15,57 @@ class FriendsPage extends StatefulWidget{
 }
 
 class _FriendsPageState extends State<FriendsPage> {
+
+  String? _teamName;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadName();
+  }
+
+  Future<void> _loadName() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final teamName = prefs.getString(teamNameKey);
+
+    setState(() {
+      _teamName = "$teamName team";
+    });
+
+    print("Load_Name $_teamName" );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: _colorFromHex("660099"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_teamName ?? ""),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addPerson,
+        tooltip: 'Increment',
+        child: const Icon(Icons.plus_one),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Future<void> _addPerson() async{
+
+  }
+
+
 }
+
+
+
+
 Color _colorFromHex(String hexColor) {
   final hexCode = hexColor.replaceAll('#', '');
   return Color(int.parse('FF$hexCode', radix: 16));
--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,49 @@
 import 'package:dest/friends_page.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const String teamNameKey = "TEAM_NAME";
 
 void main() {
-  runApp(const MyApp());
+  _prepareAndRun();
+}
+
+Future<void> _prepareAndRun() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  final teamName = prefs.getString(teamNameKey);
+
+  runApp( MyApp(teamName:teamName));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+
+  final String? teamName;
+
+  const MyApp({super.key, this.teamName});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.red,),
-      home: const MyHomePage(title: 'Secret Santa'),
+      home: const MyHomePage(),
       routes: {
+        MyHomePage.routeName :(BuildContext context)=> const MyHomePage(),
         FriendsPage.routeName :(BuildContext context)=> const FriendsPage(),
       },
+        initialRoute: teamName == null? MyHomePage.routeName : FriendsPage.routeName,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+
+  static const routeName = "/mainPage";
+
+
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -41,15 +61,17 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  void _incrementGoNext() {
+  Future<void> _incrementGoNext() async{
     Navigator.of(context).pushNamed(FriendsPage.routeName);
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString(teamNameKey, _controller.text);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("Secret Santa"),
       ),
       body: Center(
         child: Column(
